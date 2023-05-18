358d327f9d0a2fd81e2842191a3474d643867464
MicaelOliveira-dev <micaeldev@hotmail.com>
2023-05-17 22:25:45 -0300

    Primeiro Commit

--- main.tf
+++ main.tf
@@ -5,19 +5,20 @@ terraform {
       version = "~> 4.16"
     }
   }
-    required_version = ">= 1.1.0"
+
+  required_version = ">= 1.1.0"
 }
 
-    provider "aws" {
-        region  = "us-west-2"
-    }
+provider "aws" {
+  region  = "us-west-2"
+}
 
 resource "aws_instance" "app_server" {
+    key_name = "iac-micael"
     ami = "ami-0fcf52bcf5db7b003"
     instance_type = "t2.micro"
-    key_name = "iac-micael"
 
-    tags = {
-        Name = "MaquinaVirtualTeste"
-    }
+  tags = {
+    Name = "MinhaMaquinaVirtual1"
+  }
 }
